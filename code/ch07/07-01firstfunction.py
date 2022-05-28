485c9b2ec028b2f675ea4d983b4eb5d2d50306e5
JangHyuck Yun <skg09203@naver.com>
2022-05-28 18:19:16 +0900

    Create 07-01firstfunction.py

---
+++ code/ch07/07-01firstfunction.py
@@ -0,0 +1,15 @@
+#20224224 윤장혁
+def hello():
+    print('\tHello, Python!')
+
+def helloint():
+    print('\t', 77)
+
+print('처음 하는 함수 호출 hello()')
+hello()
+print('함쑤 호출 hello() 이후')
+
+print(1)
+print(helloint)
+helloint()
+print(3)
